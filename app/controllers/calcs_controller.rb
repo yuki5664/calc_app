69cd806c96433c69ddf7c2e9b2f16362ac8a3010
yuki5664 <monkey.yebisu.7777@gmail.com>
2020-03-07 17:42:01 +0900

    エラーでちゃう

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,2 +1,19 @@
 class CalcsController < ApplicationController
+  def show
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    calc = params[:calc]
+
+    @result = if calc == "addtion"
+                number1 + number2
+              elsif calc == "subtraction"
+                number1 - number2
+              elsif calc == "multiiplication"
+                number1 * number2
+              elsif calc == "division"
+                number1 / number2
+              else
+                "計算できません"
+              end
+    end
 end
